1c16e5284dfeddb3552ccbce99113c370c28f2d1
YAZDANmoo72 <yazdan.mostajabaldaveh@edu.bme.hu>
2025-09-17 17:05:47 +0200

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,11 +1,11 @@
 #include <studio.h>;
 
-int main{} {
+int main{void} {
     int x;
 
     printf("Dear user!");
-    printf("Enter a number, I will print its square!");
+    printf("Enter a number, I will print its cube and add four!");
     scanf("%d", x);
-    x = x*x
+    x = x*x*x + 4;
     printf("x square is: %f", x);
 }
